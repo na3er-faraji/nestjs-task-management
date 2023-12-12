4d255708d946d8cf729d387990dd3cdc7a0326e2
nasser faraji <faraji@bictory.io>
2023-12-12 14:48:20 +0330

    feat: import auth module inside task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { Task } from './task.entity';
 import { TasksController } from './tasks.controller';
 import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   providers: [TasksService, TasksRepository],
   controllers: [TasksController],
 })
